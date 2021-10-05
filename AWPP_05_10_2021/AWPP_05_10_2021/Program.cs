51d13c7ac976c17d0288786039996d0482198e2a
zKortex <76268639+zKortex@users.noreply.github.com>
2021-10-05 15:00:36 +0200

    AWPP 05.10.2021

---
+++ AWPP_05_10_2021/AWPP_05_10_2021/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AWPP_05_10_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // auto FordFocus = new auto(100,100,100,100,100);
+            auto FordFocus = new auto();
+            FordFocus.tanken(5.0);
+            FordFocus.pflegen(1000);
+            
+            Console.WriteLine(FordFocus.starten());
+            Console.Read();
+        }
+    }
+}
---
+++ AWPP_05_10_2021/AWPP_05_10_2021/auto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace AWPP_05_10_2021
+{
+    class auto
+    {
+
+        /*
+           double dLeistung;
+           double dKilometerstand;
+           int iBaujahr;
+           int iGewicht;
+           double dTankstand;
+
+           public auto(double Leistung, double kilometerstand,int Baujahr, int Gewicht, double Tankstand)
+           {
+               dKilometerstand = kilometerstand;
+               dLeistung = Leistung;
+               iBaujahr = Baujahr;
+               iGewicht = Gewicht;
+               dTankstand = Tankstand;
+           }
+        */
+
+
+        private double dLeistung;
+        private double dKilometerstand;
+        private int iBaujahr;
+        private int iGewicht;
+        private double dTankstand;
+
+        public void tanken(double Menge)
+        {
+            dTankstand = dTankstand + Menge;
+            pflegen(1000);
+        }
+
+         public void pflegen(int Dauer)
+         {
+
+         }
+
+         public bool starten()
+         {
+            if (dTankstand == 0)
+            {
+                return false;
+            }
+            return true;
+         }
+
+
+        void beschleunigen(int Menge)
+            {
+
+            }
+
+
+            void bremsen()
+            {
+
+            }
+        
+    }
+}
