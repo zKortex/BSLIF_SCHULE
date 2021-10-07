2fb048b86e376bed2191021f98fd8b219140737c
zKortex <76268639+zKortex@users.noreply.github.com>
2021-10-07 12:51:00 +0200

    USB Racketenwerfer

---
+++ USB_Rackentwerfer/USB_Rackentwerfer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace USB_Rackentwerfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Werfer USB_Rackentwerfer = new Werfer();
+            while (true)
+            {
+            Console.WriteLine("USB Werfer Menu");
+            Console.WriteLine("Was wollen Sie tuen? \n1. Nach Rechts drehen.\n2. Nach Links drehen.\n3. Nach Oben drehen.\n4. Nach Oben drehen.\n5. Schießen.\n6. Nachladen.\n7. System Infos.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Um viele Grad wollen Sie nach rechts drehen?");
+                        USB_Rackentwerfer.Rechts(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                    case 2:
+                        Console.WriteLine("Um viele Grad wollen Sie nach Links drehen?");
+                        USB_Rackentwerfer.Links(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                    case 3:
+                        Console.WriteLine("Um viele Grad wollen Sie nach Oben drehen?");
+                        USB_Rackentwerfer.Auf(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+
+                    case 4:
+                        Console.WriteLine("Um viele Grad wollen Sie nach Unten drehen?");
+                        USB_Rackentwerfer.Ab(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                    case 5:
+                        Console.WriteLine("Wie weit ist das Ziel Entfernt?");
+                        USB_Rackentwerfer.Feuern();
+                        //USB_Rackentwerfer.Ab(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+                    case 6:
+                        Console.WriteLine("Wollen Sie nachladen?");
+                        USB_Rackentwerfer.Feuern();
+                        //USB_Rackentwerfer.Ab(Convert.ToInt32(Console.ReadLine()));
+                    break;
+
+
+                    case 7:
+                        Console.WriteLine("Munition:" + USB_Rackentwerfer.GetMunition());
+                        Console.WriteLine("Richtung:" + USB_Rackentwerfer.GetRichtung());
+                        Console.WriteLine("Azimut:" + USB_Rackentwerfer.GetAzimut());
+                    break;
+
+                    default:
+                    break;
+                }
+
+
+
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ USB_Rackentwerfer/USB_Rackentwerfer/Werfer.cs
@@ -0,0 +1,177 @@
+﻿using System;
+namespace USB_Rackentwerfer
+{
+    class Werfer
+    {
+        private int iRichtung = 180;
+        private int iAzimut = 45;
+        private int iMunition = 4;
+
+
+
+        public int Rechts(int iGrad)
+        {
+           int iNewRotation = iRichtung + iGrad;
+           int iRestgrad = 0;
+            if (GetSpannung() >= 5)
+            {
+
+            
+            if (iNewRotation > 0 && iNewRotation < 360)
+             {
+                iRichtung = iNewRotation;
+                return iRichtung;
+             }
+            else
+            {
+                for (int i = iRichtung; i < 360; i++)
+                {
+                    iRestgrad++;
+                }
+                Console.WriteLine("Der Werfer kann nur noch " + iRestgrad + " Grade Rechts bewegt werden!");
+            }
+            }
+            else
+            {
+                Console.WriteLine("Aktion nicht möglich! Spannungsschwankung erkannt!");
+            }
+            return iRichtung;
+        }
+
+        public int Links(int iGrad)
+        {
+            int iNewRotation = iRichtung - iGrad;
+            int iRestgrad = 0;
+            if (GetSpannung() > 5)
+            {
+                if (iNewRotation > 0 && iNewRotation < 360)
+                {
+                    iRichtung = iNewRotation;
+                    return iRichtung;
+                }
+                else
+                {
+                    for (int i = iRichtung; i > 0; i--)
+                    {
+                        iRestgrad++;
+                    }
+                    Console.WriteLine("Der Werfer kann nur noch " + iRestgrad + " Grade Rechts bewegt werden!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aktion nicht möglich! Spannungsschwankung erkannt!");
+            }
+            return iRichtung;
+        }
+
+        public int Auf(int iGrad)
+        {
+            int iNewAzimut = iAzimut + iGrad;
+            int iRestAzimut = 0;
+            if (GetSpannung() > 5)
+            {
+                if (iNewAzimut > 0 && iNewAzimut < 90)
+                {
+                    iAzimut = iNewAzimut;
+                    return iAzimut;
+                }
+                else
+                {
+                    for (int i = iAzimut; i < 90; i++)
+                    {
+                        iRestAzimut++;
+                    }
+                    Console.WriteLine("Der Werfer kann nur noch " + iRestAzimut + "Grade noch oben verschoben werden!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aktion nicht möglich! Spannungsschwankung erkannt!");
+            }
+                return iAzimut;
+        }
+
+        public int Ab(int iGrad)
+        {
+            int iNewAzimut = iAzimut - iGrad;
+            int iRestAzimut = 0;
+            if (GetSpannung() > 5)
+            {
+                if (iNewAzimut > 0 && iNewAzimut < 90)
+                {
+                    iAzimut = iNewAzimut;
+                    return iAzimut;
+                }
+                else
+                {
+                    for (int i = iAzimut; i > 0; i--)
+                    {
+                        iRestAzimut++;
+                    }
+                    Console.WriteLine("Der Werfer kann nur noch " + iRestAzimut + "Grade noch oben verschoben werden!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aktion nicht möglich! Spannungsschwankung erkannt!");
+            }
+        return iAzimut;
+        }
+
+        public bool Feuern()
+        {
+            if (GetSpannung() >= 5)
+            {
+                if (iMunition >0)
+                {
+                    int distance = 100;
+                    if (distance > 0.5)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool laden()
+        {
+            if (GetSpannung() >= 5)
+            {
+                if (iMunition < 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public double GetDistance()
+        {
+            return 100;
+        }
+
+        public int GetMunition()
+        {
+            return iMunition;
+        }
+
+        public double GetRichtung()
+        {
+            return iRichtung;
+        }
+
+        public double GetAzimut()
+        {
+            return iAzimut;
+        }
+
+        private double GetSpannung()
+        {
+            Random Spannung = new Random();
+            double dspannung_v = Convert.ToDouble(Spannung.Next(48, 56)) / 10;
+           return dspannung_v;
+        }
+    }
+}
